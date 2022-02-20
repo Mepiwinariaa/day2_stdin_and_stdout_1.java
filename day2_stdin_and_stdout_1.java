4029a9dc51bb280e11b119e9efdb4a7acebaaae1
Mepiwinariaa <99885658+Mepiwinariaa@users.noreply.github.com>
2022-02-20 20:18:27 +0700

    day2_stdin_and_stdout_1.java

---
+++ day2_stdin_and_stdout_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x= scan.nextInt();
+        int y= scan.nextInt();
+        int z= scan.nextInt();
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
